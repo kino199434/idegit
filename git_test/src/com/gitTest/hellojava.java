1e5c2b1a435c798b83fc5bbde4e9873335767f69
kino199434 <664543329@qq.com>
2020-08-02 16:56:04 +0800

    将本地工程添加到git

--- git_test/src/com/gitTest/hellojava.java
+++ git_test/src/com/gitTest/hellojava.java
@@ -1,7 +1,8 @@
 package com.gitTest;
 
-public class helloproject {
+public class hellojava {
+
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hellojava");
     }
 }
